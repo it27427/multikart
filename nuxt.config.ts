e08ae27922ebb054e67f4bfb2856e3b90d73738f
it2427 <istiak.tushar.qtec@gmail.com>
2025-09-19 03:40:53 +0600

    PrimeVue Merge With Tailwind CSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,17 @@ import { fileURLToPath } from "url";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-
   modules: ["@primevue/nuxt-module"],
+
+  primevue: {
+    unstyled: true,
+  },
+
+  css: ["primeicons/primeicons.css", "@/assets/scss/main.scss"],
 
   alias: {
     "@": resolve(__dirname, "./app"),
     "@/": resolve(fileURLToPath(import.meta.url), "./app"),
     "~": resolve(fileURLToPath(import.meta.url), "./"),
   },
-});
+} as any);
